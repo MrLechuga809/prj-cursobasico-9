7106884b2f4ebed3b347ca26dc5d4d033bd29d0a
eleo95 <e.leonardo7474@gmail.com>
2018-07-31 02:42:16 -0400

    Big changes, bugfixes, NEW icon, fix theme, code cleaning

--- app/src/main/java/com/eleo95/magic8/MainActivity.java
+++ app/src/main/java/com/eleo95/magic8/MainActivity.java
@@ -1,50 +1,84 @@
 package com.eleo95.magic8;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
     private SensorManager sManager;
     private Sensor acclSensor;
     private long lastUpdate = 0;
+    private int lastRand = 0; //guarda el random in anterior
     private float x0, y0, z0;
     private static final int SHAKE_THRESHOLD = 250;
     private String[] responses;
-    private Animation a;
+    private Animation fadeAnim;
     private TextView texto;
-
+    private EditText question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         texto = findViewById(R.id.hellotext);
-        a = AnimationUtils.loadAnimation(this, R.anim.text_fade);
+        Button cpybtn = findViewById(R.id.copyButton);
+        question = findViewById(R.id.editText);
+        fadeAnim = AnimationUtils.loadAnimation(this, R.anim.text_fade); //load anim
+        //-----------------iniciando el  accelerómetro ------------------
         sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         responses = getResources().getStringArray(R.array.response_arr);
         if (sManager != null) {
             acclSensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             sManager.registerListener(this, acclSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
+        //-------------------------------------------------------------
+            cpybtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Si el editText esta vacio, no permite copiar la info
+                    if(!question.getText().toString().matches("")){
+                        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                        ClipData toCopy = ClipData.newPlainText( "Conversation", getResources().getText(R.string.Me)
+                                +question.getText().toString()
+                                +"\n"+getResources().getText(R.string.app_name)+": "
+                                +texto.getText().toString());
+                        if(clipboard != null){
+                            clipboard.setPrimaryClip(toCopy);
+                            Snackbar snack = Snackbar.make(v, R.string.textCopied,Snackbar.LENGTH_LONG);
+                            snack.getView().setBackgroundColor(getResources().getColor(R.color.grisOscuro));
+                            snack.show();
+                            question.setText("");
+                        }
 
+                    }else{
+                        Toast.makeText(MainActivity.this, R.string.questionFault, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            });
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         Sensor mySensor = event.sensor;
-        Random rand = new Random();
-        //TextView texto = findViewById(R.id.hellotext);
-        a.reset();
+        fadeAnim.reset();
 
 
         if(mySensor.getType() == Sensor.TYPE_ACCELEROMETER){
@@ -52,20 +86,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             float y = event.values[1];
             float z = event.values[2];
 
-            //sampleando el sensor a gusto
+            //se determina la velocidad para samplear el sensor
             long cTime = System.currentTimeMillis();
 
             if((cTime-lastUpdate)>100){
                 long periodo = cTime-lastUpdate;
                 lastUpdate = cTime;
 
-                float speed = Math.abs((x+y+z-x0-y0-z0)/periodo) * 1000;
+                float speed = Math.abs((x+y+z-x0-y0-z0)/periodo) * 1000; // v=d/t
 
                 if(speed > SHAKE_THRESHOLD){
-
-                    texto.setText(responses[generateRandom(rand.nextInt(19))]);
+                    //Guarda el ultimo num. generado para compararlo con el siguiente
+                    int generateRand = generateRandom(lastRand);
+                    texto.setText(responses[generateRand]);
+                    lastRand=generateRand;
                     texto.clearAnimation();
-                    texto.startAnimation(a);
+                    texto.startAnimation(fadeAnim);
 
                 }
                 x0 = x;
@@ -93,9 +129,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sManager.registerListener(this, acclSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+
+    //funcion para evitar que random genere el mismo numero pasado
     int generateRandom(int lastRandomNumber){
-        Random random = new Random(); //test
-        int randomNumber = random.nextInt(19 - 1) + 1;
+        Random random = new Random();
+      // int randomNumber = random.nextInt(19 - 1) + 1;
+        int randomNumber = random.nextInt(19);
         if(randomNumber == lastRandomNumber)
         {
             if(randomNumber==0){
@@ -103,7 +142,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }else{
                 randomNumber--;
             }
-
         }
         return randomNumber;
     }
--- app/src/main/java/com/eleo95/magic8/StartActivity.java
+++ app/src/main/java/com/eleo95/magic8/StartActivity.java
@@ -1,15 +1,15 @@
 package com.eleo95.magic8;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.LightingColorFilter;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.Objects;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -33,29 +33,35 @@ public class StartActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        final AlertDialog.Builder aDialog = new AlertDialog.Builder(this);
-        aDialog.setTitle(R.string.app_name);
+        final AlertDialog.Builder aDialog;
+        //Manejo del style si es menor a Sdk20
+        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT_WATCH){
+            aDialog = new AlertDialog.Builder(this, AlertDialog.THEME_HOLO_DARK);
+        }else{
+            aDialog = new AlertDialog.Builder(this, R.style.DarkDialogTheme);
+        }
+
+        aDialog.setTitle(R.string.exitTitle);
         aDialog.setMessage(R.string.exit);
-        aDialog.setPositiveButton(R.string.response9, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
         aDialog.setNegativeButton(R.string.no, new DialogInterface.OnClickListener(){
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
         });
+
+        aDialog.setPositiveButton(R.string.response9, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        Dialog diag = aDialog.show();
+        TextView txtvw = diag.findViewById(android.R.id.message);
+        txtvw.setInputType(R.font.product_sans);
+        txtvw.setTextColor(getResources().getColor(R.color.blanco));
         aDialog.create();
-        Objects.requireNonNull(
-                aDialog.show()
-                .getWindow())
-            .getDecorView()
-            .getBackground()
-            .setColorFilter(
-                    new LightingColorFilter(0xFF000000,0xFF454545));
 
 
     }
